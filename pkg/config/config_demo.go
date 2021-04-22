b5111497706e076018502391a81c48265a425f7a
Tran Hau <ngtranhau@gmail.com>
2021-04-22 09:25:07 +0000

    refactor: split real and demo config

--- pkg/config/config_demo.go
+++ pkg/config/config_demo.go
@@ -3,3 +3,17 @@ package config
 type configDemo struct {
 	configApps
 }
+
+var _ Config = (*configDemo)(nil)
+
+func (cd *configDemo) Install() error {
+	return nil
+}
+
+func (cd *configDemo) Update() error {
+	return nil
+}
+
+func (cd *configDemo) Clean() error {
+	return nil
+}
