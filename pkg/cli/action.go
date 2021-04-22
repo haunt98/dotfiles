992bba6640977428c06a9a59dc6a6829a520a1e6
Nguyen Tran Hau <hauvipapro@gmail.com>
2021-04-22 17:07:19 +0700

    refactor: move completely cli outside golang (#7)
    Co-authored-by: Tran Hau <ngtranhau@gmail.com>

---
+++ pkg/cli/action.go
@@ -0,0 +1,93 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/haunt98/dotfiles/pkg/config"
+	"github.com/urfave/cli/v2"
+)
+
+type action struct {
+	flags struct {
+		verbose bool
+		dryRun  bool
+	}
+}
+
+// Show help by default
+func (a *action) RunHelp(c *cli.Context) error {
+	return cli.ShowAppHelp(c)
+}
+
+func (a *action) RunInstall(c *cli.Context) error {
+	a.getFlags(c)
+	a.log("start %s\n", installCommand)
+
+	cfg, err := a.loadConfig()
+	if err != nil {
+		return err
+	}
+
+	if err := cfg.Install(); err != nil {
+		return fmt.Errorf("failed to install config: %w", err)
+	}
+
+	return nil
+}
+
+func (a *action) RunUpdate(c *cli.Context) error {
+	a.getFlags(c)
+	a.log("start %s\n", updateCommand)
+
+	cfg, err := a.loadConfig()
+	if err != nil {
+		return err
+	}
+
+	if err := cfg.Update(); err != nil {
+		return fmt.Errorf("failed to update config: %w", err)
+	}
+
+	return nil
+}
+
+func (a *action) RunClean(c *cli.Context) error {
+	a.getFlags(c)
+	a.log("start %s\n", cleanCommand)
+
+	cfg, err := a.loadConfig()
+	if err != nil {
+		return err
+	}
+
+	if err := cfg.Clean(); err != nil {
+		return fmt.Errorf("failed to clean config: %w", err)
+	}
+
+	return nil
+}
+
+func (a *action) loadConfig() (config.Config, error) {
+	cfgReal, cfgDemo, err := config.LoadConfig(currentDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	if a.flags.dryRun {
+		return cfgDemo, nil
+	}
+
+	return cfgReal, nil
+}
+
+func (a *action) getFlags(c *cli.Context) {
+	a.flags.verbose = c.Bool(verboseFlag)
+	a.flags.dryRun = c.Bool(dryRunFlag)
+}
+
+func (a *action) log(format string, v ...interface{}) {
+	if a.flags.verbose {
+		log.Printf(format, v...)
+	}
+}
