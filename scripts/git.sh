28b8580d9cb563ed186e06b3a8e7e2f7d57f42fd
Hau Nguyen <hauvipapro@gmail.com>
2021-08-14 09:35:38 +0700

    chore: add scripts folder

---
+++ scripts/git.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Aliases
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.cl clone
+git config --global alias.co checkout
+git config --global alias.df diff
+git config --global alias.lg log
+git config --global alias.pl pull
+git config --global alias.ps push
+git config --global alias.st status
+git config --global alias.sw switch
+
+# Misc
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global init.defaultBranch main
