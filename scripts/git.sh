df18c6e4598b405364b29bb34f5df5e7435bdae2
Hau Nguyen <hauvipapro@gmail.com>
2022-09-07 21:04:26 +0700

    chore(config): delta line numbers

--- scripts/git.sh
+++ scripts/git.sh
@@ -29,3 +29,4 @@ git config --global push.autoSetupRemote true
 git config --global core.pager delta
 git config --global interactive.diffFilter "delta --color-only"
 git config --global delta.navigate true
+git config --global delta.line-numbers true
