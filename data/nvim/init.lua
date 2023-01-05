17417d666c5577dbc871b23e65e980343fcce4ca
Hau Nguyen <hauvipapro@gmail.com>
2023-01-05 15:06:44 +0700

    chore(config): stylua neovim init.lua

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -121,8 +121,8 @@ require("packer").startup(function()
 	-- https://github.com/junegunn/fzf.vim
 	use("junegunn/fzf.vim")
 
-    -- https://github.com/tpope/vim-surround
-    use("tpope/vim-surround")
+	-- https://github.com/tpope/vim-surround
+	use("tpope/vim-surround")
 
 	-- Colorschemes
 	-- https://github.com/cocopon/iceberg.vim
