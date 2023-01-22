22c005929e13be308771a5dafce0d8d5bdec92ce
Hau Nguyen <hauvipapro@gmail.com>
2023-01-22 21:06:31 +0700

    chore: replace delta with difftastic

--- scripts/git.sh
+++ scripts/git.sh
@@ -27,12 +27,3 @@ git config --global pull.rebase true
 git config --global pull.twohead ort
 git config --global push.autoSetupRemote true
 git config --global rebase.stat true
-
-# Delta
-# https://github.com/dandavison/delta
-git config --global core.pager delta
-git config --global interactive.diffFilter "delta --color-only"
-git config --global delta.navigate true
-git config --global delta.line-numbers true
-git config --global include.path "~/.config/delta/themes.gitconfig"
-git config --global delta.features "chameleon"
