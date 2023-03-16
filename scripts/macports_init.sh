f98b981c6abecd456792db6da460a4072ec04891
Hau Nguyen <hauvipapro@gmail.com>
2023-03-16 15:30:23 +0700

    chore: init pfetch

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 port install \
-    git neovim \
-    fd bat ripgrep difftastic exa zellij fdupes rclone restic tealdeer task \
-    mold android-platform-tools
+	git neovim \
+	fd bat ripgrep difftastic exa zellij fdupes rclone restic tealdeer task \
+	mold android-platform-tools
