fc0cc7793ba3517c3d3d1eb39dea7f566ef24fe0
Hau Nguyen <hauvipapro@gmail.com>
2023-04-27 23:40:12 +0700

    feat: more toys for port

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -2,5 +2,5 @@
 
 port install \
 	git neovim \
-	fd bat ripgrep difftastic git-delta exa zellij fdupes rclone restic tealdeer task gdu duf \
-	android-platform-tools
+	fd bat ripgrep difftastic git-delta exa zellij fdupes rclone restic tealdeer task gdu duf nnn imagemagick ffmpegthumbnailer \
+	android-platform-tools scrcpy
