bb63129e5136a13fab35fe3b4b958eb6c1adc7cb
Hau Nguyen <hauvipapro@gmail.com>
2023-06-30 10:24:18 +0700

    chore(nvim): change theme again

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -96,11 +96,15 @@ require("lazy").setup({
 	-- Colorscheme
 	-- https://github.com/folke/tokyonight.nvim
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		"projekt0n/github-nvim-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.api.nvim_command("colorscheme tokyonight")
+			require("github-theme").setup({
+				-- ...
+			})
+
+			vim.cmd("colorscheme github_dark_dimmed")
 		end,
 	},
 
