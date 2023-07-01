48a6b7d789ade35c6bd6d2e3f69de939ce9d845f
Hau Nguyen <hauvipapro@gmail.com>
2023-07-01 15:38:19 +0700

    feat: add lua language server

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -4,4 +4,4 @@ port install \
 	git neovim \
 	fd bat ripgrep difftastic git-delta exa zellij fdupes rclone restic tealdeer task gdu duf glow \
 	nnn imagemagick ffmpegthumbnailer asciinema agg android-platform-tools scrcpy \
-	yq jq
+	lua-language-server yq jq
