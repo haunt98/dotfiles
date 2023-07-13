e22234acde328c22942b6d25abddc6e6b9ff01a6
Hau Nguyen <hauvipapro@gmail.com>
2023-07-13 21:49:04 +0700

    chore(nvim): use tab, s-tab to cmp

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -191,7 +191,27 @@ require("lazy").setup({
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm(),
+					["<CR>"] = function(fallback)
+						if cmp.visible() then
+							cmp.confirm()
+						else
+							fallback()
+						end
+					end,
+					["<Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end,
+					["<S-Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end,
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
