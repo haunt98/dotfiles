ac452694d9a86add3cb5ddc19efd13bd104370bd
Hau Nguyen <hauvipapro@gmail.com>
2023-07-16 19:32:22 +0700

    chore(macports): more useful

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -3,5 +3,5 @@
 port install \
 	git neovim \
 	fd bat ripgrep difftastic git-delta exa zellij fdupes rclone restic tealdeer task gdu duf glow \
-	nnn imagemagick ffmpegthumbnailer asciinema agg android-platform-tools scrcpy \
+	nnn imagemagick ffmpegthumbnailer asciinema agg android-platform-tools scrcpy libjxl yt-dlp \
 	lua-language-server yq jq
