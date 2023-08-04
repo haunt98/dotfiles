57821d5e4ef2dd05bb3cbe9c432f3aa4306eee01
Hau Nguyen <hauvipapro@gmail.com>
2023-08-05 01:48:43 +0700

    chore(nvim): auto dark/light color

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -80,13 +80,28 @@ require("lazy").setup({
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "mocha",
 				integrations = {
 					mini = true,
 				},
 			})
+		end,
+	},
 
-			vim.cmd("colorscheme catppuccin")
+	-- https://github.com/f-person/auto-dark-mode.nvim
+	{
+		"f-person/auto-dark-mode.nvim",
+		config = function()
+			require("auto-dark-mode").setup({
+				update_interval = 1000,
+				set_dark_mode = function()
+					vim.api.nvim_set_option("background", "dark")
+					vim.cmd("colorscheme catppuccin-mocha")
+				end,
+				set_light_mode = function()
+					vim.api.nvim_set_option("background", "light")
+					vim.cmd("colorscheme catppuccin-latte")
+				end,
+			})
 		end,
 	},
 
