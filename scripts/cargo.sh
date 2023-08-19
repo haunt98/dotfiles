cbdd2d3105f7b29d0923bfde565e3f0b17bcec1c
Hau Nguyen <hauvipapro@gmail.com>
2023-08-19 18:25:12 +0700

    chore: randomly show pokemon

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -8,3 +8,6 @@ cargo install \
 	pfetch pokeget
 
 pokeget pikachu >data/pokeget/pikachu.txt
+pokeget clefairy >data/pokeget/clefairy.txt
+pokeget ditto >data/pokeget/ditto.txt
+pokeget chikorita >data/pokeget/chikorita.txt
