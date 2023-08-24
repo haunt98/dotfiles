b3c8aff56a48f8b9d7f63cdc47a3eef24f13303e
Hau Nguyen <hauvipapro@gmail.com>
2023-08-24 15:28:20 +0700

    chore: remove luals

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -4,4 +4,4 @@ port install \
 	git neovim \
 	fd bat ripgrep git-delta exa zellij fdupes rsync rclone restic tealdeer task \
 	nnn imagemagick ffmpegthumbnailer asciinema agg android-platform-tools scrcpy libjxl yt-dlp \
-	pipx lua-language-server plantuml
+	pipx plantuml
