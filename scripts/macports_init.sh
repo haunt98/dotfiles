611c1bfe613d666d0523cd36760d89d5d3d8ad94
Hau Nguyen <hauvipapro@gmail.com>
2023-09-11 23:40:20 +0700

    chore: exa require libgit2

--- scripts/macports_init.sh
+++ scripts/macports_init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 port install \
+	libgit2 \
 	git neovim \
 	fdupes rsync rclone restic task \
 	nnn imagemagick ffmpegthumbnailer asciinema agg android-platform-tools scrcpy libjxl yt-dlp \
