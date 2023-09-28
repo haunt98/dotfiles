23bc339ee9ac6c891c802fa516a94a1a37b788fb
Hau Nguyen <hauvipapro@gmail.com>
2023-09-28 15:38:39 +0700

    chore(scripts): install libgit2 for exa, ...

---
+++ scripts/cargo_force.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cargo install --force \
+    fd-find bat ripgrep git-delta exa tealdeer \
+    difftastic vivid \
+    fnm stylua taplo-cli \
+    pfetch pokeget
--- scripts/nix_init.sh
+++ scripts/nix_init.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# https://nixos.org/manual/nix/stable/package-management/basic-package-mgmt
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --list
 nix-channel --update
 
 nix-env -iA \
-    nixpkgs.git nixpkgs.neovim \
+    nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
     nixpkgs.pipx nixpkgs.plantuml
