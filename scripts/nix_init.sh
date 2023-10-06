5539c53047264092c769f68c63ba00e28106a06d
Hau Nguyen <hauvipapro@gmail.com>
2023-10-06 14:08:10 +0700

    chore(scripts): more nix goodies

--- scripts/nix_init.sh
+++ scripts/nix_init.sh
@@ -3,7 +3,7 @@
 nix-channel --update
 
 nix-env -iA \
-    nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
+    nixpkgs.less nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
-    nixpkgs.clang-tools nixpkgs.pipx nixpkgs.plantuml
+    nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml
