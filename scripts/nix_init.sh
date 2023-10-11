70a2fd1e86beae36e4d27d66217d58bd0464b5b2
Hau Nguyen <hauvipapro@gmail.com>
2023-10-11 14:28:03 +0700

    chore(scripts): nix curl

--- scripts/nix_init.sh
+++ scripts/nix_init.sh
@@ -3,7 +3,8 @@
 nix-channel --update
 
 nix-env -iA \
-    nixpkgs.less nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
+    nixpkgs.less nixpkgs.curl \
+    nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
     nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml
--- scripts/nix_maintain.sh
+++ scripts/nix_maintain.sh
@@ -5,7 +5,8 @@ sudo -i sh -c 'nix-env -uA nixpkgs.nix'
 sudo -i sh -c 'nix-collect-garbage -d'
 
 nix-env -uA \
-    nixpkgs.less nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
+    nixpkgs.less nixpkgs.curl \
+    nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
     nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml
