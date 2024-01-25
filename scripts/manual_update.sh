f5c8c8de332ca2a038aff0cfa8db2dee9f1a912a
Hau Nguyen <hauvipapro@gmail.com>
2024-01-25 17:49:41 +0700

    chore(scripts): improve fzf

--- scripts/manual_update.sh
+++ scripts/manual_update.sh
@@ -5,4 +5,4 @@ git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull
 git -C ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull
 
 git -C ~/.fzf pull
-~/.fzf/install --all
+~/.fzf/install --key-bindings --no-completion --update-rc --no-bash --no-fish
