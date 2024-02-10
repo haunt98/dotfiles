4476dcf0addd5b7f84e0f137305952b21dce2ff6
Hau Nguyen <hauvipapro@gmail.com>
2024-02-10 22:28:48 +0700

    chore(scripts): add newsboat

--- scripts/nix_init.sh
+++ scripts/nix_init.sh
@@ -6,6 +6,6 @@ nix-env -iA \
     nixpkgs.less nixpkgs.curl \
     nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
-    nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
+    nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl nixpkgs.newsboat \
     nixpkgs.btop nixpkgs.cpufetch nixpkgs.onefetch \
     nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml nixpkgs.gh
--- scripts/nix_maintain.sh
+++ scripts/nix_maintain.sh
@@ -8,7 +8,7 @@ nix-env -uA \
     nixpkgs.less nixpkgs.curl \
     nixpkgs.libgit2 nixpkgs.git nixpkgs.neovim \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
-    nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl \
+    nixpkgs.nnn nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl nixpkgs.newsboat \
     nixpkgs.btop nixpkgs.cpufetch nixpkgs.onefetch \
     nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml nixpkgs.gh
 
