58770d4090dffff5428276d22aeea3d733c65608
Hau Nguyen <hauvipapro@gmail.com>
2024-02-14 00:54:25 +0700

    chore: add yazi to replace nnn
    https://github.com/sxyazi/yazi

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -6,7 +6,7 @@ cargo install --quiet --locked \
     fd-find bat git-delta eza sd tealdeer \
     difftastic vivid zellij zoxide atuin \
     fnm stylua taplo-cli \
-    pfetch pokeget \
+    pfetch yazi-fm pokeget \
     daktilo kbt
 
 cargo install --quiet --locked ripgrep --features 'pcre2'
