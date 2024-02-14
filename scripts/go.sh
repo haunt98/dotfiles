a2886d73218307c03ae9dc8a9c8a7fe01850a36b
Hau Nguyen <hauvipapro@gmail.com>
2024-02-14 15:37:49 +0700

    chore: remove glow and more go goodies

--- scripts/go.sh
+++ scripts/go.sh
@@ -3,7 +3,6 @@
 go install github.com/ayoisaiah/f2/cmd/f2@latest
 go install github.com/dundee/gdu/v5/cmd/gdu@latest
 go install github.com/muesli/duf@latest
-go install github.com/charmbracelet/glow@latest
 
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
 
@@ -13,9 +12,16 @@ go install github.com/mikefarah/yq/v4@latest
 go install github.com/itchyny/gojq/cmd/gojq@latest
 go install github.com/antonmedv/fx@latest
 
+go install golang.org/x/telemetry/cmd/gotelemetry@latest
+gotelemetry on
+
+go install golang.org/x/vuln/cmd/govulncheck@latest
+go install golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@latest
+
 go install mvdan.cc/gofumpt@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go install golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@latest
+go install github.com/maruel/panicparse/v2@latest
 go install gotest.tools/gotestsum@latest
+go install github.com/bufbuild/buf/cmd/buf@latest
 
 go install github.com/ankitpokhrel/jira-cli/cmd/jira@latest
