c6691243a03aab7fd9de33c032e790908d5109a0
Hau Nguyen <hauvipapro@gmail.com>
2024-02-16 11:18:47 +0700

    chore: remove plantuml

--- scripts/nix_init.sh
+++ scripts/nix_init.sh
@@ -8,4 +8,4 @@ nix-env -iA \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl nixpkgs.newsboat \
     nixpkgs.btop nixpkgs.cpufetch nixpkgs.onefetch \
-    nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml nixpkgs.gh
+    nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.gh
--- scripts/nix_maintain.sh
+++ scripts/nix_maintain.sh
@@ -10,6 +10,6 @@ nix-env -uA \
     nixpkgs.fdupes nixpkgs.rsync nixpkgs.rclone nixpkgs.restic nixpkgs.taskwarrior \
     nixpkgs.ffmpegthumbnailer nixpkgs.asciinema-agg nixpkgs.yt-dlp nixpkgs.libjxl nixpkgs.newsboat \
     nixpkgs.btop nixpkgs.cpufetch nixpkgs.onefetch \
-    nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.plantuml nixpkgs.gh
+    nixpkgs.clang-tools nixpkgs.python3 nixpkgs.pipx nixpkgs.gh
 
 nix-collect-garbage -d --quiet
