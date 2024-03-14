de0a49f11602bfed7f16ede16812fa669c1fa3d1
Hau Nguyen <hauvipapro@gmail.com>
2024-03-15 01:49:47 +0700

    chore(scripts): add fzf-tab to manual update script

--- scripts/manual_update.sh
+++ scripts/manual_update.sh
@@ -3,6 +3,7 @@
 git -C ~/.oh-my-zsh pull
 git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull
 git -C ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull
+git -C ~/.oh-my-zsh/custom/plugins/fzf-tab pull
 
 git -C ~/.fzf pull
 ~/.fzf/install --key-bindings --completion --update-rc --no-bash --no-fish
