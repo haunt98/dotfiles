9be8e77ce256c983c9ca8fdb5d1923e400443f93
Hau Nguyen <hauvipapro@gmail.com>
2024-04-05 00:52:08 +0700

    config(nvim): experiment with go.nvim

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -590,4 +590,19 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+	-- https://github.com/ray-x/go.nvim
+	{
+		"ray-x/go.nvim",
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+	},
 })
