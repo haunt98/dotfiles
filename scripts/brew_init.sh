235527d1b7a2c32bbbf78241e93bf7b65c1e900b
Hau Nguyen <hauvipapro@gmail.com>
2024-04-29 01:28:12 +0700

    chore: format

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 brew install \
-	libgit2 pcre2 xz \
-	git neovim \
-	fdupes rsync rclone restic taskwarrior-tui \
-	yt-dlp newsboat \
-	btop \
-	clang-format pipx marksman moar \
-	qmk
+    libgit2 pcre2 xz \
+    git neovim \
+    fdupes rsync rclone restic taskwarrior-tui \
+    yt-dlp newsboat \
+    btop \
+    clang-format pipx marksman moar \
+    qmk
