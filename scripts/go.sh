34a3d950ed09aee05662aedefa305a047b17a1ba
Hau Nguyen <hauvipapro@gmail.com>
2024-05-17 10:57:34 +0700

    chore: freeze buf v1.31.0 for now

--- scripts/go.sh
+++ scripts/go.sh
@@ -21,7 +21,7 @@ go install mvdan.cc/gofumpt@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/maruel/panicparse/v2@latest
 go install gotest.tools/gotestsum@latest
-go install github.com/bufbuild/buf/cmd/buf@latest
+go install github.com/bufbuild/buf/cmd/buf@v1.31.0
 
 go install github.com/ankitpokhrel/jira-cli/cmd/jira@latest
 
