bf897d1ae7b30e6eef5bd5b9629d4640b729fda8
Hau Nguyen <hauvipapro@gmail.com>
2024-08-08 14:57:19 +0700

    chore: clear log file

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,3 +2,4 @@
 # go clean -cache -testcache -modcache -fuzzcache -x
 # sudo cargo +nightly clean gc -Z gc --max-download-age=1day
 # uv cache clean
+# sudo truncate -s 0 /private/var/log/ovpnagent.log
