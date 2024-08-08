5a2faf2cd42cb98750572d812659bb5da88300c2
Hau Nguyen <hauvipapro@gmail.com>
2024-08-08 12:37:29 +0700

    chore

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -3,9 +3,8 @@
 brew install \
     libgit2 pcre2 xz \
     git neovim \
-    fdupes rsync rclone restic wcurl \
-    yt-dlp newsboat vips \
-    btop \
+    fdupes rsync rclone restic wcurl ncdu \
+    yt-dlp newsboat vips btop \
     clang-format marksman asciidoctor ollama gitleaks \
     qmk \
     redis mysql-client
