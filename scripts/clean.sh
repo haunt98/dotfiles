5c2f56ac4042daeae06fb60078172ec196c0bac0
Hau Nguyen <hauvipapro@gmail.com>
2024-08-13 01:17:34 +0700

    chore: clean ollama

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,4 +3,5 @@
 # sudo cargo +nightly clean gc -Z gc --max-download-age=1hour
 # uv cache clean
 # docker system prune --all --force --volumes
+# rm -rf ~/.ollama/models/blobs/sha256-*
 # sudo truncate -s 0 /private/var/log/ovpnagent.log
