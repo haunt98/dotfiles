f176472bd109277dc295e9de905c5efdae2ccce6
Hau Nguyen <hauvipapro@gmail.com>
2024-08-22 01:54:23 +0700

    chore: add gosec

--- scripts/go.sh
+++ scripts/go.sh
@@ -15,11 +15,12 @@ go install github.com/antonmedv/fx@latest
 go install golang.org/x/telemetry/cmd/gotelemetry@latest
 gotelemetry on
 
-go install golang.org/x/vuln/cmd/govulncheck@latest
-go install golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@latest
-go install github.com/dkorunic/betteralign/cmd/betteralign@latest
 go install mvdan.cc/gofumpt@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install golang.org/x/vuln/cmd/govulncheck@latest
+go install github.com/securego/gosec/v2/cmd/gosec@latest
+go install golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@latest
+go install github.com/dkorunic/betteralign/cmd/betteralign@latest
 go install github.com/maruel/panicparse/v2@latest
 go install gotest.tools/gotestsum@latest
 go install github.com/bufbuild/buf/cmd/buf@latest
