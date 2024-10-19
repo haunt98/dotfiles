d8453bbe23bf65afd201cf3e8339e94e83c799e3
Hau Nguyen <hauvipapro@gmail.com>
2024-10-19 23:09:32 +0700

    chore: batman

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -4,6 +4,7 @@ brew install \
     libgit2 pcre2 xz telnet \
     starship zsh-autosuggestions \
     git neovim \
+    bat-extras \
     fdupes rsync rclone restic wcurl ncdu \
     tlrc vivid zellij totp-cli \
     yt-dlp newsboat vips btop \
