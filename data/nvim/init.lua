45ef79e4245b3fe1af7ac2ca55ba63080226b226
Hau Nguyen <hauvipapro@gmail.com>
2025-03-31 15:43:32 +0700

    config(nvim): clean up

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -153,7 +153,7 @@ require("lazy").setup({
 					}
 				end,
 				integrations = {
-					treesitter_context = false,
+					blink_cmp = true,
 				},
 			})
 
@@ -612,26 +612,9 @@ require("lazy").setup({
 				end,
 			})
 
-			-- Chaos
-			-- https://www.reddit.com/r/neovim/comments/18teetv/one_day_you_will_wake_up_and_choose_the_chaos
-			-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
-			-- https://neovim.io/doc/user/diagnostic.html#diagnostic-highlights
-			-- https://emojipedia.org/
-			local signs = {
-				Error = "🔥",
-				Warn = "😤",
-				Info = "🤔",
-				Hint = "🐼",
-			}
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
-
 			-- https://neovim.io/doc/user/diagnostic.html#diagnostic-api
 			vim.diagnostic.config({
 				underline = false,
-				virtual_lines = true,
 			})
 		end,
 	},
