f106e83a131a9a36e7d0b458fab8cd246240fe9e
Hau Nguyen <hauvipapro@gmail.com>
2025-05-03 16:52:44 +0700

    chore: re-add lua-language-server

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -9,7 +9,7 @@ brew install \
     tlrc totp-cli \
     yt-dlp vips btop \
     fnm oven-sh/bun/bun deno uv \
-    just typst asciidoctor clang-format stylua sqlfluff taplo typos-cli typstyle marksman tinymist \
+    just typst asciidoctor clang-format stylua sqlfluff taplo typos-cli typstyle lua-language-server marksman tinymist \
     yq gojq fx jd ollama llama.cpp llm gitleaks gh glab tbls ghz k6 jira-cli bitwarden-cli \
     qmk/qmk/qmk asciinema qpdf fastfetch \
     redis mysql-client
