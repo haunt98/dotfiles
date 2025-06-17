08c71c5e7b6819c41849f97dcf8f555d8f24999b
Hau Nguyen <hauvipapro@gmail.com>
2025-06-17 21:14:11 +0700

    config(zsh): enable GOTELEMETRY

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
+go install golang.org/x/telemetry/cmd/gotelemetry@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest
 go install golang.org/x/tools/gopls@latest
-
-go install golang.org/x/telemetry/cmd/gotelemetry@latest
-gotelemetry on
 
 go install mvdan.cc/gofumpt@latest
 go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
