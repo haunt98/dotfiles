90ff4c028514ffab2e07c36b8a777958589b6423
Hau Nguyen <hauvipapro@gmail.com>
2025-06-23 23:31:24 +0700

    chore: rm deno

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -9,7 +9,7 @@ brew install \
     rsync rclone restic ncdu fclones f2 tlrc totp-cli \
     yt-dlp vips \
     qmk/qmk/qmk btop asciinema qpdf fastfetch laishulu/homebrew/macism \
-    fnm oven-sh/bun/bun deno uv \
+    fnm oven-sh/bun/bun uv \
     just typst asciidoctor clang-format shfmt stylua sqlfluff taplo typos-cli typstyle lua-language-server marksman tinymist \
     yq gojq fx jd llama.cpp gitleaks gh glab tbls ghz k6 bitwarden-cli \
     redis mysql-client confluentinc/tap/cli
