0912eed78e97f5763e31faedebd57f0d1f477bcd
Hau Nguyen <hauvipapro@gmail.com>
2025-07-05 11:54:13 +0700

    chore

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,8 @@
 # Need to run this once in a while
 # go clean -cache -testcache -modcache -fuzzcache -x
-# sudo cargo +nightly clean gc -Z gc --max-download-age=1hour
 # uv cache clean
 # bun pm cache rm --global
 # docker system prune --all --force --volumes
-# rm -rf ~/.ollama/models/blobs/sha256-*
 # rm -rf ~/Library/Caches/llama.cpp/*
 # sudo truncate -s 0 /private/var/log/ovpnagent.log
 # rm ~/.local/state/nvim/log
