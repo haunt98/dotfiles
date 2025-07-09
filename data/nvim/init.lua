e31ea7a71d4728c07bebe0bb2d8a58dc7479e180
Hau Nguyen <hauvipapro@gmail.com>
2025-07-09 15:18:38 +0700

    config(nvim): support range format

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -541,11 +541,19 @@ require("lazy").setup({
 					},
 				})
 
-				vim.keymap.set("n", "<Space>f", function()
-					conform.format({
-						async = true,
-					})
-				end)
+				vim.api.nvim_create_user_command("ConformFormat", function(args)
+					local range = nil
+					if args.count ~= -1 then
+						local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+						range = {
+							start = { args.line1, 0 },
+							["end"] = { args.line2, end_line:len() },
+						}
+					end
+					conform.format({ async = true, range = range })
+				end, { range = true })
+
+				vim.keymap.set({ "n", "v" }, "<Space>f", ":ConformFormat<CR>")
 			end,
 		},
 
