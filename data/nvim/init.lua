e30ec2c355505256332f415af94050322be2559f
Hau Nguyen <hauvipapro@gmail.com>
2025-08-03 00:50:43 +0700

    config(nvim): enable float transparent
    See https://github.com/catppuccin/nvim/commit/b313c962cf94416dc4b297ab517f47b0cd8272ad

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -127,6 +127,10 @@ require("lazy").setup({
 				require("catppuccin").setup({
 					flavour = "mocha",
 					transparent_background = true,
+					float = {
+						transparent = true,
+						solid = true,
+					},
 					custom_highlights = function(colors)
 						return {
 							-- Help my eyes
