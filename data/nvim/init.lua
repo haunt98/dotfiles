33a40347ac2fe540d4fcdbfa9dd6cf9741b233e7
Hau Nguyen <hauvipapro@gmail.com>
2025-09-16 12:55:01 +0700

    config(nvim): disable auto menu for blink.cmp

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -184,8 +184,12 @@ require("lazy").setup({
 							auto_insert = true,
 						},
 					},
+					menu = {
+						auto_show = false,
+					},
 					documentation = {
 						auto_show = true,
+						auto_show_delay_ms = 500,
 					},
 				},
 				sources = {
