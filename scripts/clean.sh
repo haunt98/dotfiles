cd58b86a7ceb37942832ec05ae4bd4f5e89121a9
Hau Nguyen <hauvipapro@gmail.com>
2025-10-10 00:21:10 +0700

    chore: clean more

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,8 @@
 # Need to run this once in a while
 # go clean -cache -testcache -modcache -fuzzcache -x
 # golangci-lint cache clean
+# rm -rf ~/Library/Caches/gopls
+# rm -rf ~/Library/Caches/goimports
 # uv cache clean
 # bun pm cache rm --global
 # docker system prune --all --force --volumes
