8d6325dec8ab6cd23fdf1e3a38159c7944ec805d
Hau Nguyen <hauvipapro@gmail.com>
2025-11-01 09:47:11 +0700

    chore: add air for r

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -10,6 +10,6 @@ brew install \
     yt-dlp vips \
     qmk/qmk/qmk btop asciinema qpdf fastfetch scrcpy android-platform-tools \
     fnm oven-sh/bun/bun uv \
-    just typst clang-format shfmt stylua sqlfluff taplo typos-cli typstyle lua-language-server marksman tinymist \
+    just typst clang-format shfmt stylua sqlfluff taplo typos-cli typstyle lua-language-server marksman tinymist air \
     yq gojq fx jd llama.cpp gitleaks gh glab tbls ghz k6 bitwarden-cli \
     redis mysql-client confluentinc/tap/cli
