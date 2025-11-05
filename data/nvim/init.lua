7d0e51a55aff37fc4d927b3739ec79e1a3ee97ef
Hau Nguyen <hauvipapro@gmail.com>
2025-11-05 10:07:43 +0700

    chore: cleanup unused

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -608,11 +608,6 @@ require("lazy").setup({
 				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
 				vim.lsp.enable("lua_ls")
 
-				-- Markdown
-				-- https://github.com/artempyanykh/marksman
-				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#marksman
-				vim.lsp.enable("marksman")
-
 				-- Typst
 				-- https://github.com/Myriad-Dreamin/tinymist
 				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#tinymist
