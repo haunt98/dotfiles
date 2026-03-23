8aa897cc49de385a2b7deea971a76976e739feef
Hau Nguyen <hauvipapro@gmail.com>
2026-03-23 22:30:01 +0700

    chore

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -4,7 +4,7 @@ brew install \
     less xz telnet \
     zsh-autosuggestions \
     git git-filter-repo neovim tree-sitter-cli \
-    fzf fd bat ripgrep git-delta difftastic eza zoxide starship sd moor \
+    fzf fd bat ripgrep git-delta eza zoxide starship sd moor \
     rsync rclone restic ncdu fclones f2 tlrc totp-cli \
     ffmpeg yt-dlp vips jpeg-xl \
     qpdf fastfetch android-platform-tools \
--- scripts/git.sh
+++ scripts/git.sh
@@ -70,11 +70,3 @@ git config --global delta.features catppuccin-mocha
 git config --global delta.catppuccin-mocha.file-decoration-style none
 git config --global delta.catppuccin-mocha.hunk-header-decoration-style none
 git config --global delta.catppuccin-mocha.commit-decoration-style none
-
-# https://github.com/Wilfred/difftastic
-git config --global alias.dftc "difftool --cached"
-git config --global alias.dft "difftool"
-git config --global diff.tool difftastic
-git config --global difftool.difftastic.cmd 'difft "$LOCAL" "$REMOTE"'
-git config --global difftool.prompt false
-git config --global pager.difftool true
