e01fd782e03fbd6232597090ad8c931cb2abe7ad
Hau Nguyen <hauvipapro@gmail.com>
2026-04-28 17:25:13 +0700

    chore: re-add govulncheck

--- scripts/go.sh
+++ scripts/go.sh
@@ -4,6 +4,7 @@ go install golang.org/x/telemetry/cmd/gotelemetry@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest
 go install golang.org/x/tools/gopls@latest
+go install golang.org/x/vuln/cmd/govulncheck@latest
 
 go install mvdan.cc/gofumpt@latest
 go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
