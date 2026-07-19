985f92a21bfb2efc71103d198bac340a830b8863
Hau Nguyen <hauvipapro@gmail.com>
2026-07-20 01:47:43 +0700

    chore: rm semgrep again

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -3,11 +3,11 @@
 brew install \
     bash less xz telnet \
     zsh-autosuggestions \
-    git git-filter-repo neovim tree-sitter-cli \
+    git git-filter-repo neovim vim-classic tree-sitter-cli \
     fzf fd bat ripgrep git-delta eza zoxide starship sd moor \
     rsync rclone restic ncdu fclones f2 tlrc totp-cli \
     ffmpeg yt-dlp vips jpeg-xl \
     qpdf fastfetch android-platform-tools \
     fnm just typst clang-format shfmt stylua sqlfluff taplo typos-cli typstyle lua-language-server tinymist air \
-    yq gojq fx jd gh glab tbls k6 \
+    yq gojq fx jd xan gh glab tbls k6 \
     redis
