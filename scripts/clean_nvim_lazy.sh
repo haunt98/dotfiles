001ed07e272e6b16b0b37a0fa0c03bf38dba8974
Hau Nguyen <hauvipapro@gmail.com>
2026-08-08 13:42:20 +0700

    chore(scripts): clean lazy nvim

---
+++ scripts/clean_nvim_lazy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+LAZY_DIR="${HOME}/.local/share/nvim/lazy"
+
+if [[ ! -d $LAZY_DIR ]]; then
+    echo "$LAZY_DIR not exist"
+    exit 1
+fi
+
+for dir in "$LAZY_DIR"/*/; do
+    if [[ -d $dir && -d "${dir}.git" ]]; then
+        plugin_name=$(basename "$dir")
+        echo "Clean $plugin_name"
+
+        git -C "$dir" clean -dfx
+        git -C "$dir" gc
+    fi
+done
