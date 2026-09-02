fa8e25445f47ec9df398b94b039958922ae6be16
Hau Nguyen <hauvipapro@gmail.com>
2026-09-02 16:48:27 +0700

    config(nvim): update treesitter

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -585,7 +585,7 @@ require("lazy").setup({
 		-- https://github.com/nvim-treesitter/nvim-treesitter
 		{
 			"nvim-treesitter/nvim-treesitter",
-			commit = "074aa4422bf029908338e855d0c0f71470a971bb",
+			commit = "427e9222363d07c32d6db6169e4049c28d58d141",
 			build = ":TSUpdate",
 			config = function()
 				local ts = require("nvim-treesitter")
