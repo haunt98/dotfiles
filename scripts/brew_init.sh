def86c96ab055cb72d838e053f17629a4de6a37c
Hau Nguyen <hauvipapro@gmail.com>
2026-09-14 15:29:55 +0700

    chore(scripts): more optional

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -7,7 +7,6 @@ brew install \
     fzf fd bat ripgrep git-delta eza zoxide starship sd \
     rsync rclone restic ncdu fclones f2 tlrc totp-cli \
     ffmpeg yt-dlp vips jpeg-xl \
-    qpdf fastfetch android-platform-tools \
-    fnm just typst clang-format shfmt stylua sqlfluff taplo typos-cli typstyle lua-language-server tinymist air \
-    yq gojq fx jd xan gh glab tbls k6 \
-    redis
+    qpdf fastfetch \
+    fnm just clang-format shfmt stylua sqlfluff taplo typos-cli lua-language-server \
+    yq gojq fx jd xan gh glab tbls k6
