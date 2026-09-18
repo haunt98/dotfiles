466679eca009b279d33321ae541711a83167e76b
Hau Nguyen <hauvipapro@gmail.com>
2026-09-19 00:19:20 +0700

    chore

--- scripts/brew_init.sh
+++ scripts/brew_init.sh
@@ -6,7 +6,7 @@ brew install \
     git git-filter-repo tree-sitter-cli \
     fzf fd bat ripgrep git-delta eza zoxide starship sd \
     rsync rclone restic ncdu fclones f2 tlrc totp-cli \
-    ffmpeg yt-dlp vips jpeg-xl \
+    ffmpeg yt-dlp vips jpeg-xl exiftool \
     qpdf fastfetch \
     uv fnm just clang-format shfmt prettier stylua sqlfluff taplo typos-cli lua-language-server \
     yq gojq fx jd xan gh glab tbls k6
